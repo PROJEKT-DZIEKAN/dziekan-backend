8065a6eee9742cc9cbe0a33f1209bf6e7302f10e
Nicolas Graeb <97298829+NicolasGraeb@users.noreply.github.com>
2025-07-03 01:43:09 +0200

    Update DziekanApplication.java

--- src/main/java/com/pbs/app/DziekanApplication.java
+++ src/main/java/com/pbs/app/DziekanApplication.java
@@ -1,6 +1,5 @@
 package com.pbs.app;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,14 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DziekanApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure()
-                              .ignoreIfMissing()
-                              .load();
-
-        dotenv.entries().forEach(entry ->
-            System.setProperty(entry.getKey(), entry.getValue())
-        );
-
         SpringApplication.run(DziekanApplication.class, args);
     }
+
 }
