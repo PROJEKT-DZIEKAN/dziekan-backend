2f281caeae6a21096630913363a18ea2325b6ae8
NicolasGraeb <nicolasgraeb2@gmail.com>
2025-07-14 22:00:06 +0200

    Finalize notifications feature (security, properties etc.)

--- src/main/java/com/pbs/app/config/SecurityConfig.java
+++ src/main/java/com/pbs/app/config/SecurityConfig.java
@@ -36,6 +36,8 @@ public class SecurityConfig {
               "/api/auth/**",
               "/api/refresh-token",
               "/swagger-ui.html",
+                "/swagger-ui/index.html",
+                "/api/qr/**",
               "/v3/api-docs/**"
             ).permitAll()
             .requestMatchers("/ws-chat/**").permitAll()
